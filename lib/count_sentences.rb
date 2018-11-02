0ed6770cd674e857b88787e0aaaa3c6bf37a5187
oazzat <oazzat19@gmail.com>
2018-11-02 09:14:34 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[self.length - 1] == "."
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[self.length - 1] == "?"
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[self.length - 1] == "!"
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    counter = 0
+    prev = ""
+    self.each_char do |letter|
+      if letter == "." || letter == "?" || letter == "!"
+        counter +=1
+        if prev == "." || prev == "?" || prev == "!"
+          counter -= 1
+        end
+      end
+      prev = letter
+    end
+    counter
   end
 end
